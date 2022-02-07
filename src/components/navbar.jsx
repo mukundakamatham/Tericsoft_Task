f4978a2acb3d65e8e98fc6e9ddb49e2fcf13081c
MUKUNDA <mukundakamatham@gmail.com>
2022-02-07 14:19:20 +0000

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,1 +1,43 @@
-import {react}
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+export const Navbar1=()=>{
+
+
+    return (
+        <div id="navbar">
+<div>
+        <FontAwesomeIcon icon="fa-duotone fa-star" />
+      
+        </div> 
+        <div>
+        <FontAwesomeIcon icon="fa-duotone fa-star" />
+      
+        </div>
+        <div>
+        <FontAwesomeIcon icon="fa-duotone fa-star" />
+      
+        </div>
+        <div>
+        <FontAwesomeIcon icon="fa-duotone fa-star" />
+      
+        </div>
+        <div>
+        <FontAwesomeIcon icon="fa-duotone fa-star" />
+      
+        </div>
+        <div>
+        <FontAwesomeIcon icon="fa-duotone fa-star" />
+      
+        </div>
+        <div>
+        <FontAwesomeIcon icon="fa-duotone fa-star" />
+      
+        </div>
+        
+        
+        
+        
+        
+         </div>
+    )
+}
